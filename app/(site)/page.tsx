33a1e5c02fc9311dc2995096aee09957c28bf852
rishabhkum-Dev-1799 <rishabhkum17@gmail.com>
2023-06-24 12:23:03 +0530

    "feature- Header and Main Components for the whole App"

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,22 @@
+import Header from '@/components/Header/Header';
+import ListItem from '@/components/Home/ListItem';
 export default function Home() {
-  return <div className='text-green-500'>Placeholder Text</div>;
+  return (
+    <div className='bg-neutral-900 h-full w-full rounded-lg overflow-hidden overflow-y-auto'>
+      <Header>
+        <div className='mb-2'>
+          <h1 className='text-white text-3xl font-semibold'>Welcome back</h1>
+          <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 mt-4 gap-3'>
+            <ListItem name='LikedSongs' href='liked' />
+          </div>
+        </div>
+      </Header>
+      <div className='mt-2 mb-7 px-6 '>
+        <div className='flex justify-between items-center'>
+          <h1 className='text-white text-2xl font-semibold'>Newest Songs</h1>
+        </div>
+        <div>List of Songs</div>
+      </div>
+    </div>
+  );
 }
---
+++ components/Home/ListItem.tsx
@@ -0,0 +1,34 @@
+'use client';
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import { FaPlay } from 'react-icons/fa';
+import likedImage from '../../public/images/liked.png';
+interface ListItemProps {
+  name: string;
+  href: string;
+}
+const ListItem: React.FC<ListItemProps> = ({ name, image, href }) => {
+  const router = useRouter();
+  //handler functions
+  const likeHandler = () => {
+    // Add authentication before pushing it
+    router.push(href);
+  };
+  return (
+    <button
+      onClick={likeHandler}
+      className='flex items-center gap-x-4 rounded-md relative group bg-neutral-100/10 hover:bg-neutral-100/20 transistion cursor-pointer pr-4 overflow-hidden'
+    >
+      <div className='relative min-h-[64px] min-w-[64px]'>
+        <Image src={likedImage} alt='like_song' fill className='object-cover' />
+      </div>
+      <p className='text-white font-medium truncate py-5'>{name}</p>
+      <div className='  absolute right-5 flex items-center justify-center rounded-full p-4 bg-indigo-500 opacity-0 group-hover:opacity-100 hover:scale-110 transition'>
+        <FaPlay className='text-black' />
+      </div>
+    </button>
+  );
+};
+
+export default ListItem;
